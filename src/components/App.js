54096ee127d3eae70b98289e9d69b25b4acb3ef8
VictorAnizar <victoranizarmorales@gmail.com>
2021-10-06 20:05:15 -0500

    convirtiendo el archivo App en clase en vez de funcion

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,25 @@ import Header from './Header';
 import Footer from './Footer';
 import ContentMain from './ContentMain'
 import TodoList from './TodoList';
+import { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="layout">
-      
-      <Header border={"white"}/>
-      <ContentMain/>
-      {/* Si una etiqueta no tiene hijos, se coloca como la siguiente, 
-      en caso contrario: se debe cerrar como una etiqueta normal de html */}
-      <TodoList/>
-      <Footer year={2021} >Pie</Footer>
-    </div>
-  );
-
+class App extends Component {
+  render(){
+    return (
+      <div className="layout">
+        
+        <Header border={"white"}/>
+        <ContentMain/>
+        {/* Si una etiqueta no tiene hijos, se coloca como la siguiente, 
+        en caso contrario: se debe cerrar como una etiqueta normal de html */}
+        <TodoList/>
+        <Footer year={2021} >Pie</Footer>
+      </div>
+    );
+  
+  }
+  
 }
 
 export default App;
